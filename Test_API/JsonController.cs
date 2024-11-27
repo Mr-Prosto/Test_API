2c0cba53987009241adaa74d1b77dc9c09e6ed46
Mr_Prosto <dluaobycheniua@yandex.ru>
2024-11-27 18:37:24 +0300

    test_api

---
+++ Test_API/JsonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Test_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JsonController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] string json)
+        {
+            // Process the incoming JSON
+            var result = JsonSerializer.Deserialize<dynamic>(json);
+
+            // Here you would typically process the JSON and generate a response
+            // For this example, we'll just return the input JSON
+
+            return Ok(result);
+        }
+    }
+}
